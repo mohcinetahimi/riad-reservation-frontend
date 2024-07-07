b1417605ea5c7d8e57fbb35a4e961732b6a94b08
mohcinetahimi <mohcinetahimi@gmail.com>
2024-07-07 19:08:15 +0100

    home page with simple  date picker need fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Register from './components/Auth/Register'
 import Login from './components/Auth/Login'
 import Example from './components/Auth/Example'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route,BrowserRouter } from 'react-router-dom'
+import Home from './pages/home/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Home/>}/> 
       <Route path='/register' element={<Register />}/>
       <Route path='/login' element={<Login />}/>
     </Routes>
+    </BrowserRouter>
+    
     </>
     
   )
